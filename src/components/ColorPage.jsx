c83976059d0122c590a229d99a7d172de175c299
june <wnsgmlb501@naver.com>
2025-05-26 23:56:21 +0900

    fin

--- src/components/ColorPage.jsx
+++ src/components/ColorPage.jsx
@@ -2,9 +2,13 @@ import { useParams } from 'react-router-dom';
 import '../styles/ColorPage.css';
 
 function ColorPage() {
-
+  const {id} = useParams();
+  const bg ={
+    background: id,
+    justifyContent:"center"
+  };
   return (
-    <div className="color-page">
+    <div className="color-page" style={bg}>
     배경 페이지입니다 
     </div>
   );
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,24 @@
 import { useNavigate } from 'react-router-dom';
 import '../styles/MainPage.css';
 
+
 const Mainpage = () => {
- 
+  let clrs=['red','blue','yellow','pink','orange'];
+  const navigate = useNavigate();
+  function BtnClick (e){
+    navigate(`/color/${clrs[e.target.id]}`);
+  }
+
+
   return (
     <div className="container">
       <h2>React 색상 선택</h2>
-      <hr />
+      <hr/>
+      {clrs.map((e,index)=>
+        <button id={index} className='color-box' onClick={BtnClick} style={{background:e}}>
+          {e}
+        </button>
+      )}
     </div>
   );
 };
